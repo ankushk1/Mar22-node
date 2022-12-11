7336c4077c302d6fbf09a4b0a8d9e5957ea44b49
Ankush Kapoor <ankushkapoor179@gmail.com>
2022-12-11 13:27:06 +0530

    initialized nodejs server

---
+++ index.js
@@ -0,0 +1,52 @@
+const express = require("express");
+const app = express();
+const bodyparser = require("body-parser");
+const port = 8000;
+
+let arr = [];
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.send("<h1>Hey from Nodejs server</h1>");
+});
+
+//app."Method"("/route", func/controller)
+app.post("/create", (req, res) => {
+  arr.push({ ...req.body, id: Math.floor(Math.random() * 1000) });
+  res.json({ message: "Object added successfully" });
+});
+
+app.get("/getData/:name", (req, res) => {
+  // console.log(req.query);
+  // console.log(req.params);
+  console.log('first');
+  res.json(arr);
+});
+
+app.get("/getData/:admin", (req, res)=> {
+  console.log('second')
+})
+
+app.delete("/deleteData", (req, res) => {
+  // Try splice on your own
+  const id = req.body.id;
+  const updatedArr = arr.filter((elem) => elem.id !== id);
+  arr = [...updatedArr];
+  res.json({ message: "Object deleted successfully" });
+});
+
+app.put("/updateData", (req, res) => {
+  const { id, name, age } = req.body;
+  const index = arr.findIndex((elem) => elem.id === id);
+  arr[index] = {
+    id: id,
+    name: name,
+    age: age
+  };
+  res.json({ message: "Object updated successfully" });
+});
+
+app.listen(port, () => {
+  console.log("Server Running on 8000");
+});
