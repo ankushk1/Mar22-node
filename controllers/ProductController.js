cdb04f9b5b6f1aac2813b07fb5b6ccae15e78854
Ankush Kapoor <ankushkapoor179@gmail.com>
2023-01-08 13:02:44 +0530

    Product api's completed

--- controllers/ProductController.js
+++ controllers/ProductController.js
@@ -30,14 +30,134 @@ exports.getProducts = async (req, res) => {
   try {
     const products = await Product.find({
       $and: [{ isActive: true }, { quantity: { $gt: 0 } }]
-    }).populate("category","name")
-    .populate("created_by", "firstname email");
+    })
+      .populate("category", "name")
+      .populate("created_by", "firstname email");
 
     if (!products || !products.length) {
       return res.status(400).json({ message: "No products found" });
     }
 
     return res.status(200).json({ products: products });
+  } catch (err) {
+    return res.status(500).json({
+      error: err,
+      message: "Internal Server Error"
+    });
+  }
+};
+
+exports.getProductById = async (req, res) => {
+  try {
+    const id = req.params.id;
+
+    const product = await Product.findById(id)
+      .populate("category", "name")
+      .populate("created_by", "firstname email");
+    if (!product) {
+      return res.status(400).json({ message: "Product doesn't exist" });
+    }
+
+    const { _id, name, description, price, category } = product;
+    return res.status(200).json({ _id, name, description, price, category });
+  } catch (err) {
+    return res.status(500).json({
+      error: err,
+      message: "Internal Server Error"
+    });
+  }
+};
+
+exports.deleteProduct = async (req, res) => {
+  try {
+    const id = req.params.id;
+
+    const deletedProduct = await Product.findByIdAndDelete(id);
+
+    if (!deletedProduct) {
+      return res
+        .status(400)
+        .json({ message: "Product deletion failed/Invalid Product" });
+    }
+
+    return res.status(200).json({
+      message: "Product Deleted Successfully"
+    });
+  } catch (err) {
+    return res.status(500).json({
+      error: err,
+      message: "Internal Server Error"
+    });
+  }
+};
+
+exports.updateProduct = async (req, res) => {
+  try {
+    const id = req.params.id;
+    const updatedProduct = await Product.findByIdAndUpdate(id, {
+      $set: { ...req.body, updated_by: req.body.userId }
+    });
+
+    if (!updatedProduct) {
+      return res
+        .status(400)
+        .json({ message: "Product updation failed/Invalid Product" });
+    }
+
+    return res.status(200).json({ message: "Product Updated Successfully" });
+  } catch (err) {
+    return res.status(500).json({
+      error: err,
+      message: "Internal Server Error"
+    });
+  }
+};
+
+
+exports.updateQuantity = async (req, res) => {
+  try{
+    const id = req.params.id;
+    const product = await Product.findById(id)
+
+    if(product.quantity < req.body.quantity){
+      return res.status(400).json({message:`You cannot buy quantity of this product exceeding ${product.quantity} units`})
+    }
+
+    const updatedQuantity = await Product.findByIdAndUpdate(id, {
+      $inc : { quantity : -req.body.quantity},
+      $set : { updated_by: req.body.userId }
+    });
+
+    if (!updatedQuantity ) {
+      return res
+        .status(400)
+        .json({ message: "Quantity updation failed/Invalid Product" });
+    }
+
+    return res.status(200).json({ message: "Quantity Updated Successfully" });
+  }catch (err) {
+    return res.status(500).json({
+      error: err,
+      message: "Internal Server Error"
+    });
+  }
+}
+
+
+exports.deactivateProduct = async (req, res) => {
+  try {
+    const id = req.params.id;
+    const deactivatedProduct = await Product.findByIdAndUpdate(id, {
+      $set: { isActive: false, updated_by: req.body.userId }
+    });
+
+    if (!deactivatedProduct) {
+      return res
+        .status(400)
+        .json({ message: "Product deactivation failed/Invalid Product" });
+    }
+
+    return res.status(200).json({ message: "Product Deactivated Successfully" });
   } catch (err) {
     return res.status(500).json({
       error: err,
--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,12 +1,22 @@
 const express = require("express");
 const {
   createProduct,
-  getProducts
+  getProducts,
+  getProductById,
+  deleteProduct,
+  updateProduct,
+  updateQuantity,
+  deactivateProduct
 } = require("../controllers/ProductController");
 const { validateJWT } = require("../middleware/jwt");
 const router = express.Router();
 
 router.post("/create", validateJWT, createProduct);
 router.get("/getProducts", validateJWT, getProducts);
+router.get("/getProductById/:id", validateJWT, getProductById);
+router.delete("/delete/:id", validateJWT, deleteProduct);
+router.put("/update/:id", validateJWT, updateProduct);
+router.put("/quantityChange/:id", validateJWT, updateQuantity);
+router.put("/deactivate/:id", validateJWT, deactivateProduct);
 
 module.exports = router;
